d062f6b73c91cf8ac0cb9461802df04ecbd42539
Craig_hero <craigchihururu@gmail.com>
2022-08-11 17:53:29 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,299 @@
+function menuControll(toggle) {
+    const tl = gsap.timeline({ defualts: { ease: "power4.out" } })
+    if (toggle) {
+        tl.set(".menu-logo .cr-logo", { opacity: 0 })
+            .set(".menu-toggle .toggle-title", { opacity: 0 })
+            .set(".nav-container .cr-logo", { opacity: 1 })
+            .to(".back-drop", { duration: .2, "pointer-events": "none", opacity: 0 })
+            .to(".menu-item", { duration: .5, x: "200px", stagger: .03 }, "-=.2")
+            .to(".menu-content", { duration: .4, x: "101%" }, "-=.52")
+            .set(".menu-toggle .toggle-btn>span", { "background": "var(--black)" }, "-=.2")
+    } else {
+        tl.set(".menu-toggle .toggle-btn>span", { "background": "var(--white)" })
+            .set(".menu-logo .cr-logo", { opacity: 1 })
+            .set(".menu-toggle .toggle-title", { opacity: 1 })
+            .set(".nav-container .cr-logo", { opacity: 0 })
+            .to(".back-drop", { duration: .2, "pointer-events": "all", opacity: 1 })
+            .to(".menu-content", { duration: .4, x: "0%" }, "-=.2")
+            .to(".menu-item", { duration: .5, x: "0px", stagger: .03 }, "-=.28")
+    }
+}
+
+function menu_btn(active) {
+    const tl = gsap.timeline({ defaults: { duration: 0.3 } })
+
+    if (active) {
+        tl.to(".toggle-btn span", { rotate: 0 })
+            .to(".toggle-btn span:first-child", { y: 6 })
+            .to(".toggle-btn span:last-child", { y: 16 }, "-=.3")
+    } else {
+        tl.to(".toggle-btn span", { y: 12 })
+            .to(".toggle-btn span:first-child", { rotate: 45 })
+            .to(".toggle-btn span:last-child", { rotate: -45 }, "-=.3")
+    }
+
+}
+
+function menu() {
+    var active = false
+
+    $(".menu-toggle .toggle-btn").click(() => {
+        menuControll(active)
+        menu_btn(active)
+
+        // To-do: available when on desktop
+        scrollLock(!active)
+
+        active = active ? false : true
+    })
+}
+
+function buttonHover() {
+
+    const tl = gsap.timeline({ defaults: { duration: 0.4 } })
+
+    if ($(".btn").attr("data-fipple") == "false"){
+        console.log("locked button")
+        return
+    }
+
+    $(".btn").hover(
+        function () {
+            var text = $(this).find(".btn-text")
+            var fill = $(this).find(".btn-fill")
+
+            tl.set(fill, { transform: 'translate3d(0,76%,0)' })
+                .to(fill, { transform: 'translate3d(0,0%,0)' })
+
+            text.addClass('white')
+        },
+        function () {
+            var text = $(this).find(".btn-text")
+            var fill = $(this).find(".btn-fill")
+
+            tl.to(fill, { transform: 'translate3d(0,-76%,0)' })
+
+            text.removeClass('white')
+        }
+    )
+}
+
+function magenet() {
+    $('.magenet-area').on('mousemove', function (e) {
+        var boundingRect = $(this).get(0).getBoundingClientRect();
+        var relX = e.pageX - boundingRect.left;
+        var relY = e.pageY - boundingRect.top;
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        var movement = parseFloat($(this).attr("data-magnetic-strength"));
+
+        gsap.to($(this).siblings(), { x: (relX - boundingRect.width / 2) * movement, y: (relY - boundingRect.height / 2 - scrollTop) * movement, ease: "power1", duration: 0.4 });
+    })
+
+    $('.magenet-area').on('mouseleave', function (e) {
+        gsap.to($(this).siblings(), { x: 0, y: 0, ease: Back.easeOut, duration: 0.6 });
+    })
+}
+
+function cursor() {
+    var cursor = $(".cr-cursor")
+
+    $(".project-thumbnail").hover(
+        () => {
+            cursor.css({ "--size": "100px", "background-color": "rgba(255, 255, 255, 0.4)", "border-width": "2px" })
+            cursor.find('span').css({ "font-size": "16px", "line-height": "100%" })
+        },
+        () => {
+            cursor.css({ "--size": "7px", "background-color": "var(--black)", "border-width": "0px" })
+            cursor.find('span').css({ "font-size": "0px", "line-height": "0%" })
+        }
+    )
+
+    $(".link").hover(
+        () => {
+            cursor.css({ "--size": "60px", "background-color": "transparent", "backdrop-filter": "invert()" })
+        },
+        () => {
+            cursor.css({ "--size": "7px", "background-color": "var(--black)", "backdrop-filter": "blur(5px)" })
+        }
+    )
+
+}
+
+function menu_item() {
+    $(".menu-links li a").hover(
+        function () {
+            $(".menu-links li a").css({ "opacity": ".3" })
+            $(this).css({ "opacity": "1" })
+        },
+        function () {
+            $(".menu-links li a").css({ "opacity": "1" })
+        }
+    )
+}
+
+function inView(el, func) {
+    var element = $(el)
+
+    var top = element.offset().top - $(window).scrollTop()
+
+    var bottom = top + element.outerHeight()
+
+    if (top < $(window).height() & bottom > 0) {
+        func()
+    }
+}
+
+function emailCheck(input) {
+    var field = document.getElementsByName(input)[0]
+    var message = document.querySelector(".error-message")
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (field == null) return;
+
+    field.addEventListener("focusout", () => {
+
+        if (field.value.match(mailformat)) {
+            field.classList.remove("error")
+            message.classList.add("active")
+            console.log("all good")
+        } else {
+            field.classList.add("error")
+            message.classList.remove("active")
+            console.log("error")
+        }
+    });
+}
+
+function footerScroll() {
+    var footer = $(".footer")
+
+    if (footer.html() == undefined) return
+
+    var distance = footer.offset().top - $(window).scrollTop()
+
+    var percent = (distance / $(window).height()) * - 60
+
+    inView(
+        ".footer",
+        () => { footer.css({ "transform": `translateY(${percent}%)` }) }
+    )
+    requestAnimationFrame(footerScroll)
+}
+
+function intersect(observe, lookup, function1, function2) {
+    var lookupTop = $(lookup).offset().top
+    var lookupBottom = lookupTop + $(lookup).outerHeight()
+
+    var observeTop = $(observe).offset().top
+    var observeBottom = observeTop + $(observe).outerHeight()
+
+    var t1 = (observeTop > lookupTop & observeTop < lookupBottom)
+    var t2 = (observeBottom > lookupTop & observeBottom < lookupBottom)
+
+    if (t1 | t2) {
+        function1()
+    } else {
+        function2()
+    }
+
+}
+
+function parallax() {
+    $(".screenshot-preview").each((i, div) => {
+
+        if($(div).attr("data-parallax") == "true"){
+            var top = $(div).offset().top - $(window).scrollTop()
+
+            var winHeight = $(window).height()
+    
+            var divHeight = $(div).outerHeight()
+            
+            console.log("wokring")
+            
+            inView(div, () => {
+                var child = $(div).find('img');
+    
+                var x = (winHeight - 2 * top - divHeight) / (winHeight + divHeight)
+    
+                child.css({ "-webkit-transform": `translate3d(0px,${x * 30}%, 0px)`, "will-change": "transform" });
+
+                console.log("parallax")
+            })
+        }
+
+        
+    })
+
+    requestAnimationFrame(parallax)
+}
+
+function checkbox() {
+    $(".checkbox-btn").click(function () {
+        var b = $(this).siblings("input").prop('checked')
+
+        var btn = $(this)
+
+        var text = $(this).find(".btn-text")
+        var fill = $(this).find(".btn-fill")
+
+        const tl = gsap.timeline({ defaults: { duration: 0.4 } })
+
+        if (b) {
+            btn.attr("data-fipple", "false")
+            console.log(b, "locked")
+            fill.css({background: "var(--black)"})
+            text.removeClass('white')
+            tl.to(fill, { transform: 'translate3d(0,-76%,0)' })
+            .set(fill, { transform: 'translate3d(0,76%,0)' })
+        } else {
+            btn.attr("data-fipple", "true")
+            fill.css({background: "var(--black)"})
+            console.log(b, "unlocked")
+            text.addClass('white')
+            
+            tl.to(fill, { transform: 'translate3d(0,0%,0)' })
+
+        }
+    })
+}
+
+$(window).on("load", () => {
+    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+        $(document).mousemove(e => {
+            var cursor = $(".cr-cursor")
+            cursor.css({ "display": "flex" })
+            gsap.to(".cr-cursor", { x: e.clientX, y: e.clientY, duration: 0.1, });
+        })
+
+        setupAnimation()
+        cursor()
+        magenet()
+        buttonHover()
+        
+        
+        footerScroll()
+        parallax()
+    }
+    
+    
+    
+    
+    $(window).scroll(() => {
+        $(document).prop({ '--scroll-y': `${window.scrollY}px` })
+    })
+
+    custOnScroll(() => {
+        intersect(
+            ".menu-toggle .toggle-btn",
+            ".footer",
+            () => { $(".toggle-btn span").css({ "background": "var(--white)" }) },
+            () => { $(".toggle-btn span").css({ "background": "var(--black)" }) }
+        )
+    })
+
+    emailCheck("email");
+    
+    menu()
+    checkbox()
+    menu_item()
+});
---
+++ js/smooth-scroll.js
@@ -0,0 +1,122 @@
+let prev = 0;
+let current = 0;
+let target = 0;
+let ease = 0.13;
+let currentDis = 0;
+let scroll = true;
+let isScrolling;
+
+let cointainer = '.scroll-wrap';
+let body = document.body;
+
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+function resize(el, height) {
+    el.style.height = `${height}px`
+}
+
+
+function smoothScroll() {
+    prev = current
+
+    current = parseFloat(lerp(current, target, ease).toFixed(2));
+
+    target = scroll ? window.scrollY : current;
+
+    isScrolling = (prev != current)
+
+    $(cointainer).css({'transform': `translateY(${-current}px)`})
+    
+    requestAnimationFrame(smoothScroll)
+}
+
+function scrollbar(){
+    var scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    var per = window.scrollY / scrollMaxY
+    
+    var height = ($(window).height()/scrollMaxY) * $(".scrolltrack").height()
+
+    var trackHeight = $(".scrolltrack").height() - height
+    
+    $('.scrollbar').css({'transform': `translateY(${trackHeight * per}px)`, 'height': `${height}px`})
+}
+
+function scrollWrap() {
+    $(".content-wrap").css({
+        "position": "fixed",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100vh"
+    });
+    
+    $(".scroll-wrap").css({
+        'position': 'absolute',
+        "z-index": "1",
+        'width': '100vw',
+        'will-change': 'transform',
+    })
+}
+
+function scrollLock(active){
+    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+        if(active){
+            scroll = false;
+        }else{
+            scroll = true;
+            window.scroll(0, current)
+        }
+    }else{
+        if(active){
+            const scrollY = $(document).prop('--scroll-y')
+            $(document.body).css({'position': 'fixed', 'top': `-${scrollY}`})
+        }else{
+            const scrollY = $('body').css('top')
+            $(document.body).css({'position': '', 'top': ''})
+            window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        }
+    }
+}
+
+function scrollbarOpacity(){
+    var scrollbar = $(".scrollbar")
+    if(isScrolling){
+        scrollbar.removeClass("disappear")
+    }else{
+        scrollbar.addClass("disappear")
+    }
+
+    requestAnimationFrame(scrollbarOpacity)
+}
+
+function custOnScroll(func){
+    if(isScrolling) func()
+
+    requestAnimationFrame(()=>{custOnScroll(func)})
+}
+
+function onResize(){
+    $(window).on('resize', function(){
+        resize(body, $(cointainer).outerHeight())
+    });
+}
+
+function setupAnimation() {
+    $('.scrolltrack').css({'display': 'block'})
+    scrollWrap()
+
+    resize(body, $(cointainer).outerHeight())
+    
+    smoothScroll()
+    
+    $(window).scroll(() => {
+        scrollbar()
+    })
+    
+    onResize()
+
+    scrollbarOpacity()
+}
+
