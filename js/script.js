afb87028be2bbaee9f7d8eb44862e1800f03dd9c
Craig Chihururu <46131588+craig-ckc@users.noreply.github.com>
2024-02-07 13:46:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+// Horizontal scroll
+let tlMain = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".section-height",
+      start: "top top",
+      end: "98% bottom",
+      scrub: 1,
+    },
+  })
+  .to(".section-track", { xPercent: -100, ease: "none" });
 
+// hero photo
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".panel-02",
+      containerAnimation: tlMain,
+      start: "left left",
+      end: "right left",
+      scrub: true,
+      markers: true,
+    },
+  })
+  .to(".field-img-wrap", { x: "0vw" });
+
+console.log("Wokring");
